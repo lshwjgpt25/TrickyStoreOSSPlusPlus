341aeec28ee5f85ae5867e0cfbe2107d7c40a239
Chris Renshaw <osm0sis@outlook.com>
2025-08-06 10:32:50 -0300

    Sync with official service.sh but also tidy (#8)
    Signed-off-by: Dakkshesh <beakthoven@gmail.com>

--- module/service.sh
+++ module/service.sh
@@ -4,11 +4,8 @@ MODDIR=${0%/*}
 
 cd $MODDIR
 
-(
-while [ true ]; do
-  $MODDIR/daemon "$MODDIR"
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-done
-) &
+while true; do
+  ./daemon "$MODDIR" || exit 1
+  # ensure keystore initialized
+  sleep 2
+done &
