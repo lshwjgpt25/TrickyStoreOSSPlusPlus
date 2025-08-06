3fa0435db89724e1e690ac8a942511c3f9496431
Chris Renshaw <osm0sis@outlook.com>
2025-08-06 10:32:50 -0300

    Sync with official service.sh but also tidy (#8)

--- module/template/service.sh
+++ module/template/service.sh
@@ -4,11 +4,8 @@ MODDIR=${0%/*}
 
 cd $MODDIR
 
-(
-while [ true ]; do
-  ./daemon
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-done
-) &
+while true; do
+  ./daemon || exit 1
+  # ensure keystore initialized
+  sleep 2
+done &
